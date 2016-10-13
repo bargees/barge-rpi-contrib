9f23c16face7889291b171056389d9839cb2864f
A.I <ailis@paw.zone>
2016-10-13 13:27:44 -0700

    Keep /dev/mmcblk0p2 same, even if the boot partition is bigger

--- utils/partition.sh
+++ utils/partition.sh
@@ -10,7 +10,7 @@ DEVICE=/dev/mmcblk0
 read -p "Are you sure to partition the SD card (y/n)? " -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  (echo n; echo p; echo 3; echo ; echo +1000M ; echo w;) | fdisk ${DEVICE}
+  (echo n; echo p; echo 3; echo ; echo 5655 ; echo w;) | fdisk ${DEVICE}
   (echo t; echo 3; echo 82; echo w;) | fdisk ${DEVICE}
   mkswap -L BARGE-SWAP ${DEVICE}p3
 
